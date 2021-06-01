48b7b5042da5883ca18241c3c381c4d0e145fbae
Iryna Zybliyenka <Iryna_Zybliyenka@epam.com>
2021-06-01 18:26:40 +0300

    fixed aircompany

---
+++ Net/Aircompany/Airport.cs
@@ -0,0 +1,64 @@
+﻿using Aircompany.Models;
+using Aircompany.Planes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aircompany
+{
+    public class Airport
+    {
+        public List<PlaneBase> Planes { get; private set; }
+
+        public Airport(IEnumerable<PlaneBase> planes)
+        {
+            Planes = planes.ToList();
+        }
+
+        public List<PassengerPlane> GetPassengersPlanes()
+        {
+            return Planes.Where(x => x.GetType() == typeof(PassengerPlane)).Select(x => (PassengerPlane)x).ToList();
+        }
+
+        public List<MilitaryPlane> GetMilitaryPlanes()
+        {
+            return Planes.Where(x => x.GetType() == typeof(MilitaryPlane)).Select(x => (MilitaryPlane)x).ToList();
+        }
+
+        public PassengerPlane GetPassengerPlaneWithMaxPassengersCapacity()
+        {
+            return GetPassengersPlanes().Aggregate((w, x) => w.PassengersCapacity > x.PassengersCapacity ? w : x);
+        }
+
+        public List<MilitaryPlane> GetTransportMilitaryPlanes()
+        {
+            return GetMilitaryPlanes().Where(x => x.Type == MilitaryType.TRANSPORT).ToList();
+        }
+
+        public Airport SortByMaxDistance()
+        {
+            return new Airport(Planes.OrderBy(w => w.MaxFlightDistance));
+        }
+
+        public Airport SortByMaxSpeed()
+        {
+            return new Airport(Planes.OrderBy(w => w.MaxSpeed));
+        }
+
+        public Airport SortByMaxLoadCapacity()
+        {
+            return new Airport(Planes.OrderBy(w => w.MaxLoadCapacity));
+        }
+
+        public IEnumerable<PlaneBase> GetPlanes()
+        {
+            return Planes;
+        }
+
+        public override string ToString()
+        {
+            return "Airport{" +
+                    "planes=" + string.Join(", ", Planes.Select(x => x.Model)) +
+                    '}';
+        }
+    }
+}
